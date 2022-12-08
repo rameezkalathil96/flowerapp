ea4f94847d28520e15e6da3b779ee03db4baa1a4
rameez <rameez.k@hudini.io>
2022-12-08 10:24:20 +0530

    hi

--- src/Cart.js/Cart.js
+++ src/Cart.js/Cart.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Button from './Button';
 import classes from './cart.module.css';
-// import img from '../public/assets/cartimage/Mask Group 2/Mask Group 2.png'
-
-// import VueEcommerceCard from "vue-ecommerce-card";
+import MaskGroup3x from '../assets/cartimage/Mask Group 2/MaskGroup3x.png';
 
 
 
@@ -16,6 +14,29 @@ const Cart = () => {
     <div className={classes.cartbgcolor}>
       <span className={classes.headingShop}>Shopping cart</span>
       <div className={`card ${classes.field}`}>
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img
+                src={MaskGroup3x}
+                class="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <span className={`card-title ${classes.cardtitle}`}>Soft Pink Box</span>
+                <p class="card-text">
+                  Soft colours with a very feminine touch. Delicate but elegant.
+                </p>
+                <p class="card-text">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className={classes.button}>
           <Button classbutton={classes.classbutton} button_name="PLACE ORDER" />
         </div>
