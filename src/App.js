90543ca0ca1361f4e51f3de9a332f24b5b429c5f
rameez <rameez.k@hudini.io>
2022-12-09 16:58:25 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import './App.css';
 import Cart from './Cart.js/Cart';
-<<<<<<< HEAD
 import Welcome from "./Welcome/Welcome";
 import Login from "./Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import Home from './Component/Home';
->>>>>>> master
 import CreateBouquet from './bouquet/CreateBouquet';
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
     {/* <CreateBouquet/> */}
     {/* <Cart/> */}
     {/* <Cart/> */}
-    <BrowserRouter>
+    {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-=======
     <Home/>
-    <CreateBouquet/>
+    <CreateBouquet/> */}
     <Cart/>
->>>>>>> master
     </div>
   );
 }
